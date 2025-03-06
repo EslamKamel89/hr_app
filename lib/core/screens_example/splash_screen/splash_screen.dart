2d80ac3d20c7881a2a391e8e9ea9a3bc67b6b957
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-06 11:14:20 +0200

    Refactor splash screen to use animated text widget and update logo asset

--- lib/core/screens_example/splash_screen/splash_screen.dart
+++ lib/core/screens_example/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hr/core/extensions/context-extensions.dart';
-import 'package:hr/core/router/app_routes_names.dart';
+import 'package:hr/core/widgets/animated_text_widget.dart';
+import 'package:hr/utils/assets/assets.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,23 +10,26 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _animation;
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
     // _animation = CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
-    _animation = Tween<double>(
-      begin: 0,
-      end: 1,
-    ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    _animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
     _animationController.forward();
     _animationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         Future.delayed(const Duration(seconds: 1), () {
-          Navigator.of(context).pushNamedAndRemoveUntil(AppRoutesNames.uiComponentScreen, (_) => false);
+          // Navigator.of(context).pushNamedAndRemoveUntil(AppRoutesNames.uiComponentScreen, (_) => false);
         });
       }
     });
@@ -39,34 +43,51 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Crescent moon with a star
             Stack(
               alignment: Alignment.center,
               children: [
-                Icon(
-                  Icons.view_compact_alt_rounded, // Crescent moon icon
-                  size: 100,
-                  color: context.primaryColor, // Gold color
+                Image.asset(
+                  AssetsData.logo,
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
                 ),
               ],
             ),
             const SizedBox(height: 20),
             // Animated App Title
-            FadeTransition(
-              opacity: _animation,
-              child: const Text(
-                'App Title',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
-              ),
-            ),
+            // FadeTransition(
+            //   opacity: _animation,
+            //   child: const Text(
+            //     'HR',
+            //     style: TextStyle(
+            //       fontSize: 28,
+            //       fontWeight: FontWeight.bold,
+            //       color: Colors.black,
+            //     ),
+            //   ),
+            // ),
             const SizedBox(height: 10),
             // Subtitle
-            FadeTransition(
-              opacity: _animation,
-              child: const Text(
-                'App Description',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.black54),
+            // FadeTransition(
+            //   opacity: _animation,
+            //   child: const Text(
+            //     textAlign: TextAlign.center,
+            //     style: TextStyle(
+            //       fontSize: 20,
+            //       fontWeight: FontWeight.bold,
+            //       color: Colors.black,
+            //     ),
+            //   ),
+            // ),
+            const AnimatedTextWidget(
+              text: "Connecting people, driving progress.",
+              letterAnimationDuration: Duration(milliseconds: 100),
+              recombineDuration: Duration(milliseconds: 100),
+              textStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
             ),
           ],
