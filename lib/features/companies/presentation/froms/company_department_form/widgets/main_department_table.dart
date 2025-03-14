aec7e964e2c863afbecfcaab5f0005ee9ed11d14
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-14 14:16:13 +0200

    Refactor SubDepartmentTableWidget to use ListView for improved layout; update department naming and enhance UI with action buttons for editing and deleting entries.

--- lib/features/companies/presentation/froms/company_department_form/widgets/main_department_table.dart
+++ lib/features/companies/presentation/froms/company_department_form/widgets/main_department_table.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: dead_code
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:hr/utils/styles/styles.dart';
 
 class Department {
   final int id;
@@ -17,12 +20,12 @@ class MainDepartmentTableWidget extends StatefulWidget {
 }
 
 class _MainDepartmentTableWidgetState extends State<MainDepartmentTableWidget> {
-  late MainDepartmentDataSource _dataSource;
+  // late MainDepartmentDataSource _dataSource;
 
   @override
   void initState() {
     super.initState();
-    _dataSource = MainDepartmentDataSource(_generateData());
+    // _dataSource = MainDepartmentDataSource(_generateData());
   }
 
   List<Department> _generateData() {
@@ -30,115 +33,191 @@ class _MainDepartmentTableWidgetState extends State<MainDepartmentTableWidget> {
       50,
       (index) => Department(
         id: index + 1,
-        subDepartment: "SubDept ${index + 1}",
-        mainDepartment: "MainDept ${((index) / 5).floor() + 1}",
+        subDepartment: "This is Sub Department ${index + 1}",
+        mainDepartment: "This is Main Department ${((index) / 5).floor() + 1}",
       ),
     );
   }
 
+  final borderSide = BorderSide(color: Colors.grey);
   @override
   Widget build(BuildContext context) {
-    return PaginatedDataTable(
-          // header: const Text(
-          //   "Departments",
-          //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          // ),
-          columns: const [
-            // DataColumn(label: Text("ID", style: TextStyle(fontWeight: FontWeight.bold))),
-            // DataColumn(
-            //   label: Text(
-            //     "Sub\nCategory",
-            //     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-            //     textAlign: TextAlign.center,
-            //   ),
-            // ),
-            DataColumn(
-              label: Text(
-                "Main\nCategory",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                textAlign: TextAlign.center,
+    return ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: _generateData().length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                padding: EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(border: Border(bottom: BorderSide())),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: txt('Main\nDepartment', e: St.bold16, textAlign: TextAlign.center),
+                    ),
+
+                    Expanded(
+                      flex: 1,
+                      child: txt('Action', e: St.bold16, textAlign: TextAlign.center),
+                    ),
+                  ],
+                ),
+              );
+            }
+            index = index - 1;
+            return Container(
+              margin: EdgeInsets.symmetric(vertical: 5),
+              padding: EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(border: Border(bottom: borderSide)),
+
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(left: borderSide, right: borderSide),
+                      ),
+                      child: txt(
+                        _generateData()[index].mainDepartment,
+                        e: St.reg16,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(border: Border(right: borderSide)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(onTap: () {}, child: Icon(Icons.edit, size: 25))
+                              .animate()
+                              .fadeIn(duration: const Duration(milliseconds: 300))
+                              .slideX(begin: -0.1, duration: const Duration(milliseconds: 300)),
+                          SizedBox(width: 10),
+                          InkWell(
+                                onTap: () {},
+                                child: Icon(Icons.delete, size: 25, color: Colors.redAccent),
+                              )
+                              .animate()
+                              .fadeIn(duration: const Duration(milliseconds: 300))
+                              .slideX(begin: 0.1, duration: const Duration(milliseconds: 300)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            DataColumn(
-              label: Text("Action", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
-            ),
-          ],
-          source: _dataSource,
-          rowsPerPage: 10,
-          availableRowsPerPage: const [10, 20, 50],
+            );
+          },
         )
         .animate()
         .fadeIn(duration: const Duration(milliseconds: 600))
         .slideY(begin: 0.2, duration: const Duration(milliseconds: 600));
+    // return PaginatedDataTable(
+    //       // header: const Text(
+    //       //   "Departments",
+    //       //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    //       // ),
+    //       columns: const [
+    //         // DataColumn(label: Text("ID", style: TextStyle(fontWeight: FontWeight.bold))),
+    //         // DataColumn(
+    //         //   label: Text(
+    //         //     "Sub\nCategory",
+    //         //     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+    //         //     textAlign: TextAlign.center,
+    //         //   ),
+    //         // ),
+    //         DataColumn(
+    //           label: Text(
+    //             "Main\nDepartment",
+    //             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+    //             textAlign: TextAlign.center,
+    //           ),
+    //         ),
+    //         DataColumn(
+    //           label: Text("Action", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+    //         ),
+    //       ],
+    //       source: _dataSource,
+    //       rowsPerPage: 10,
+    //       availableRowsPerPage: const [10, 20, 50],
+    //     )
+    //     .animate()
+    //     .fadeIn(duration: const Duration(milliseconds: 600))
+    //     .slideY(begin: 0.2, duration: const Duration(milliseconds: 600));
   }
 }
 
-class MainDepartmentDataSource extends DataTableSource {
-  final List<Department> departments;
-  MainDepartmentDataSource(this.departments);
+// class MainDepartmentDataSource extends DataTableSource {
+//   final List<Department> departments;
+//   MainDepartmentDataSource(this.departments);
 
-  @override
-  DataRow? getRow(int index) {
-    if (index >= departments.length) return null;
-    final dept = departments[index];
-    return DataRow.byIndex(
-      index: index,
-      cells: [
-        // DataCell(Text(dept.id.toString())),
-        // DataCell(Text(dept.subDepartment)),
-        DataCell(Text(dept.mainDepartment)),
-        DataCell(
-          Row(
-            children: [
-              InkWell(onTap: () {}, child: Icon(Icons.edit, size: 25))
-                  .animate()
-                  .fadeIn(duration: const Duration(milliseconds: 300))
-                  .slideX(begin: -0.1, duration: const Duration(milliseconds: 300)),
-              SizedBox(width: 10),
-              InkWell(onTap: () {}, child: Icon(Icons.delete, size: 25, color: Colors.redAccent))
-                  .animate()
-                  .fadeIn(duration: const Duration(milliseconds: 300))
-                  .slideX(begin: 0.1, duration: const Duration(milliseconds: 300)),
-              // ElevatedButton.icon(
-              //       onPressed: () {
-              //         // Edit action here.
-              //       },
-              //       icon: const Icon(Icons.edit, size: 16),
-              //       label: const Text("Edit", style: TextStyle(fontSize: 12)),
-              //       style: ElevatedButton.styleFrom(
-              //         // primary: Colors.blue,
-              //         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              //       ),
-              //     )
-              //     .animate()
-              //     .fadeIn(duration: const Duration(milliseconds: 300))
-              //     .slideX(begin: -0.1, duration: const Duration(milliseconds: 300)),
-              // const SizedBox(width: 8),
-              // ElevatedButton.icon(
-              //       onPressed: () {
-              //         // Delete action here.
-              //       },
-              //       icon: const Icon(Icons.delete, size: 16),
-              //       label: const Text("Delete", style: TextStyle(fontSize: 12)),
-              //       style: ElevatedButton.styleFrom(
-              //         // primary: Colors.red,
-              //         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              //       ),
-              //     )
-              //     .animate()
-              //     .fadeIn(duration: const Duration(milliseconds: 300))
-              //     .slideX(begin: 0.1, duration: const Duration(milliseconds: 300)),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
+//   @override
+//   DataRow? getRow(int index) {
+//     if (index >= departments.length) return null;
+//     final dept = departments[index];
+//     return DataRow.byIndex(
+//       index: index,
+//       cells: [
+//         // DataCell(Text(dept.id.toString())),
+//         // DataCell(Text(dept.subDepartment)),
+//         DataCell(Text(dept.mainDepartment)),
+//         DataCell(
+//           Row(
+//             children: [
+//               InkWell(onTap: () {}, child: Icon(Icons.edit, size: 25))
+//                   .animate()
+//                   .fadeIn(duration: const Duration(milliseconds: 300))
+//                   .slideX(begin: -0.1, duration: const Duration(milliseconds: 300)),
+//               SizedBox(width: 10),
+//               InkWell(onTap: () {}, child: Icon(Icons.delete, size: 25, color: Colors.redAccent))
+//                   .animate()
+//                   .fadeIn(duration: const Duration(milliseconds: 300))
+//                   .slideX(begin: 0.1, duration: const Duration(milliseconds: 300)),
+//               // ElevatedButton.icon(
+//               //       onPressed: () {
+//               //         // Edit action here.
+//               //       },
+//               //       icon: const Icon(Icons.edit, size: 16),
+//               //       label: const Text("Edit", style: TextStyle(fontSize: 12)),
+//               //       style: ElevatedButton.styleFrom(
+//               //         // primary: Colors.blue,
+//               //         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+//               //       ),
+//               //     )
+//               //     .animate()
+//               //     .fadeIn(duration: const Duration(milliseconds: 300))
+//               //     .slideX(begin: -0.1, duration: const Duration(milliseconds: 300)),
+//               // const SizedBox(width: 8),
+//               // ElevatedButton.icon(
+//               //       onPressed: () {
+//               //         // Delete action here.
+//               //       },
+//               //       icon: const Icon(Icons.delete, size: 16),
+//               //       label: const Text("Delete", style: TextStyle(fontSize: 12)),
+//               //       style: ElevatedButton.styleFrom(
+//               //         // primary: Colors.red,
+//               //         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+//               //       ),
+//               //     )
+//               //     .animate()
+//               //     .fadeIn(duration: const Duration(milliseconds: 300))
+//               //     .slideX(begin: 0.1, duration: const Duration(milliseconds: 300)),
+//             ],
+//           ),
+//         ),
+//       ],
+//     );
+//   }
 
-  @override
-  bool get isRowCountApproximate => false;
-  @override
-  int get rowCount => departments.length;
-  @override
-  int get selectedRowCount => 0;
-}
+//   @override
+//   bool get isRowCountApproximate => false;
+//   @override
+//   int get rowCount => departments.length;
+//   @override
+//   int get selectedRowCount => 0;
+// }
--- lib/features/companies/presentation/froms/company_department_form/widgets/sub_department_table.dart
+++ lib/features/companies/presentation/froms/company_department_form/widgets/sub_department_table.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:hr/features/companies/presentation/froms/company_department_form/widgets/main_department_table.dart';
+import 'package:hr/utils/styles/styles.dart';
 
 class SubDepartmentTableWidget extends StatefulWidget {
   const SubDepartmentTableWidget({super.key});
@@ -23,46 +24,137 @@ class _SubDepartmentTableWidgetState extends State<SubDepartmentTableWidget> {
       50,
       (index) => Department(
         id: index + 1,
-        subDepartment: "SubDept ${index + 1}",
-        mainDepartment: "MainDept ${((index) / 5).floor() + 1}",
+        subDepartment: "This is Sub Department ${index + 1}",
+        mainDepartment: "This is Main Department ${((index) / 5).floor() + 1}",
       ),
     );
   }
 
+  final borderSide = BorderSide(color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
-    return PaginatedDataTable(
-          // header: const Text(
-          //   "Departments",
-          //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          // ),
-          columns: const [
-            // DataColumn(label: Text("ID", style: TextStyle(fontWeight: FontWeight.bold))),
-            DataColumn(
-              label: Text(
-                "Sub\nCategory",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                textAlign: TextAlign.center,
+    return ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: _generateData().length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                padding: EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(border: Border(bottom: BorderSide())),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: txt('Main\nDepartment', e: St.bold16, textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: txt('Sub\nDepartment', e: St.bold16, textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: txt('Action', e: St.bold16, textAlign: TextAlign.center),
+                    ),
+                  ],
+                ),
+              );
+            }
+            index = index - 1;
+            return Container(
+              margin: EdgeInsets.symmetric(vertical: 5),
+              padding: EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(border: Border(bottom: borderSide)),
+
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(left: borderSide, right: borderSide),
+                      ),
+                      child: txt(
+                        _generateData()[index].mainDepartment,
+                        e: St.reg16,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(border: Border(right: borderSide)),
+                      child: txt(
+                        _generateData()[index].subDepartment,
+                        e: St.reg16,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(border: Border(right: borderSide)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(onTap: () {}, child: Icon(Icons.edit, size: 25))
+                              .animate()
+                              .fadeIn(duration: const Duration(milliseconds: 300))
+                              .slideX(begin: -0.1, duration: const Duration(milliseconds: 300)),
+                          SizedBox(width: 10),
+                          InkWell(
+                                onTap: () {},
+                                child: Icon(Icons.delete, size: 25, color: Colors.redAccent),
+                              )
+                              .animate()
+                              .fadeIn(duration: const Duration(milliseconds: 300))
+                              .slideX(begin: 0.1, duration: const Duration(milliseconds: 300)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            DataColumn(
-              label: Text(
-                "Main\nCategory",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            DataColumn(
-              label: Text("Action", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
-            ),
-          ],
-          source: _dataSource,
-          rowsPerPage: 10,
-          availableRowsPerPage: const [10, 20, 50],
+            );
+          },
         )
         .animate()
         .fadeIn(duration: const Duration(milliseconds: 600))
         .slideY(begin: 0.2, duration: const Duration(milliseconds: 600));
+    // return PaginatedDataTable(
+    //       // header: const Text(
+    //       //   "Departments",
+    //       //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    //       // ),
+    //       columns: const [
+    //         // DataColumn(label: Text("ID", style: TextStyle(fontWeight: FontWeight.bold))),
+    //         DataColumn(
+    //           label: Text(
+    //             "Sub\nDepartment",
+    //             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+    //             textAlign: TextAlign.center,
+    //           ),
+    //         ),
+    //         DataColumn(
+    //           label: Text(
+    //             "Main\nDepartment",
+    //             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+    //             textAlign: TextAlign.center,
+    //           ),
+    //         ),
+    //         DataColumn(
+    //           label: Text("Action", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+    //         ),
+    //       ],
+    //       source: _dataSource,
+    //       rowsPerPage: 10,
+    //       availableRowsPerPage: const [10, 20, 50],
+    //     )
+    //     .animate()
+    //     .fadeIn(duration: const Duration(milliseconds: 600))
+    //     .slideY(begin: 0.2, duration: const Duration(milliseconds: 600));
   }
 }
 
