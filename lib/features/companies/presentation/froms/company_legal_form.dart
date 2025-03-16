9c63c250117d39367590ef6c8522bd16f0637407
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-16 12:06:38 +0200

    Refactor CompanyLegalForm to improve state management and form handling; integrate legal data submission logic

--- lib/features/companies/presentation/froms/company_legal_form.dart
+++ lib/features/companies/presentation/froms/company_legal_form.dart
@@ -7,6 +7,7 @@ import 'package:hr/core/models/api_response_model.dart';
 import 'package:hr/core/widgets/inputs.dart';
 import 'package:hr/core/widgets/save_button.dart';
 import 'package:hr/features/companies/cubits/company_legal_cubit.dart';
+import 'package:hr/features/companies/helpers/get_company.dart';
 import 'package:hr/features/companies/models/company_legal_model.dart';
 import 'package:hr/features/companies/presentation/widgets/basic_info_filled_widget.dart';
 import 'package:hr/features/companies/presentation/widgets/form_vertical_gap.dart';
@@ -37,7 +38,7 @@ class _CompanyLegalFormState extends State<CompanyLegalForm> {
   @override
   void initState() {
     _controller = context.read<CompanyLegalCubit>();
-    // _controller.contactShow(context);
+    _controller.legalShow(context);
     super.initState();
   }
 
@@ -51,76 +52,85 @@ class _CompanyLegalFormState extends State<CompanyLegalForm> {
   Widget build(BuildContext context) {
     return CompanyBasicInfoFilledWidget(
       currentTab: 6,
-      child: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20),
-              CustomDateField(
-                label: 'Trade License Expiry Date',
-                req: true,
-                onDateSubmit: (date) {
-                  _expiryDatetime = date;
-                },
-                initialDate: parseDateTime(_controller.state.data?.tradeLicenseExpiryDate),
-                textEditingController: _tradeLicenseExpiryDate,
-                validator:
-                    (input) => valdiator(
-                      input: input,
-                      label: 'Trade License Expiry Date',
-                      isRequired: true,
-                    ),
-              ),
-              FormVerticalGap(),
-              CustomTextFormField(
-                label: 'VAT Registration Number',
-                placeholder: 'Enter VAT Registration Number',
-                controller: _vatRegistrationNumber,
-                validator:
-                    (input) => valdiator(
-                      input: input,
-                      label: 'VAT Registration Number',
-                      isRequired: true,
-                      minChars: 3,
-                      maxChars: 50,
-                    ),
-              ),
-              FormVerticalGap(),
-              CustomTextFormField(
-                label: 'Chamber of Commerce Membership Number',
-                placeholder: 'Enter Chamber of Commerce Membership Number',
-                controller: _chamberOfCommerceMembershipNumber,
-                showNumberOnly: true,
-                validator:
-                    (input) => valdiator(
-                      input: input,
-                      label: 'Chamber of Commerce Membership Number',
-                      isRequired: true,
-                      minChars: 3,
-                      maxChars: 50,
-                    ),
-              ),
-
-              SizedBox(height: 30),
-              BlocBuilder<CompanyLegalCubit, ApiCrudResponseModel<CompanyLegalModel>>(
-                builder: (context, state) {
-                  if (state.upsertResponse == ResponseEnum.loading) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  return SaveButton(
-                    onTap: () {
-                      FocusScope.of(context).unfocus();
-                      _sendRequest();
+      child: BlocBuilder<CompanyLegalCubit, ApiCrudResponseModel<CompanyLegalModel>>(
+        buildWhen: (previous, current) {
+          return (previous.showResponse != ResponseEnum.success &&
+              current.showResponse == ResponseEnum.success);
+        },
+        builder: (context, state) {
+          _initializeFields();
+          return Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  CustomDateField(
+                    label: 'Trade License Expiry Date',
+                    req: true,
+                    onDateSubmit: (date) {
+                      _expiryDatetime = date;
                     },
-                  );
-                },
+                    initialDate: parseDateTime(_controller.state.data?.tradeLicenseExpiryDate),
+                    textEditingController: _tradeLicenseExpiryDate,
+                    validator:
+                        (input) => valdiator(
+                          input: input,
+                          label: 'Trade License Expiry Date',
+                          isRequired: true,
+                        ),
+                  ),
+                  FormVerticalGap(),
+                  CustomTextFormField(
+                    label: 'VAT Registration Number',
+                    placeholder: 'Enter VAT Registration Number',
+                    controller: _vatRegistrationNumber,
+                    validator:
+                        (input) => valdiator(
+                          input: input,
+                          label: 'VAT Registration Number',
+                          isRequired: true,
+                          minChars: 3,
+                          maxChars: 50,
+                        ),
+                  ),
+                  FormVerticalGap(),
+                  CustomTextFormField(
+                    label: 'Chamber of Commerce Membership Number',
+                    placeholder: 'Enter Chamber of Commerce Membership Number',
+                    controller: _chamberOfCommerceMembershipNumber,
+                    showNumberOnly: true,
+                    validator:
+                        (input) => valdiator(
+                          input: input,
+                          label: 'Chamber of Commerce Membership Number',
+                          isRequired: true,
+                          minChars: 3,
+                          maxChars: 50,
+                        ),
+                  ),
+
+                  SizedBox(height: 30),
+                  BlocBuilder<CompanyLegalCubit, ApiCrudResponseModel<CompanyLegalModel>>(
+                    builder: (context, state) {
+                      if (state.upsertResponse == ResponseEnum.loading) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      return SaveButton(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                          _sendRequest();
+                        },
+                      );
+                    },
+                  ),
+                  SizedBox(height: 300),
+                ],
               ),
-              SizedBox(height: 300),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
@@ -140,20 +150,15 @@ class _CompanyLegalFormState extends State<CompanyLegalForm> {
 
   Future _sendRequest() async {
     if (_formKey.currentState!.validate()) {
-      // _controller.contactUpsert(
-      //   context,
-      //   CompanyContactModel(
-      //     contactName: _contactName.text,
-      //     companyEmail: _companyEmail.text,
-      //     primaryMobileNumber: _primaryMobileNumber.text,
-      //     secondaryMobileNumber: _secondaryMobileNumber.text,
-      //     landlineNumber: _landlineNumber.text,
-      //     faxNumber: _faxNumber.text,
-      //     hrMobileNumber: _hrMobileNumber.text,
-      //     hrLandlineNumber: _hrLandlineNumber.text,
-      //     hrEmail: _hrEmail.text,
-      //   ),
-      // );
+      _controller.legalUpsert(
+        context,
+        CompanyLegalModel(
+          tradeLicenseExpiryDate: _expiryDatetime?.toIso8601String(),
+          vatRegistrationNumber: _vatRegistrationNumber.text,
+          chamberOfCommerceMembershipNumber: _chamberOfCommerceMembershipNumber.text,
+          companyBasicId: getCompany(context)?.id,
+        ),
+      );
     }
   }
 }
