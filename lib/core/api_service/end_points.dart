70f58e685d09f4d1597b191ab67cf27bf5e499fa
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-03 13:34:38 +0200

    chore(config): switch API endpoints, relax context theme typing, and bump Flutter deps

--- lib/core/api_service/end_points.dart
+++ lib/core/api_service/end_points.dart
@@ -1,6 +1,8 @@
 class EndPoint {
-  static const String baseUrl = "https://hr.gaztec.org/api";
-  static const String uploadUrl = "https://hr.gaztec.org/";
+  // static const String baseUrl = "https://hr.gaztec.org/api";
+  // static const String uploadUrl = "https://hr.gaztec.org/";
+  static const String baseUrl = "http://hr.safqauae.com/api";
+  static const String uploadUrl = "http://hr.safqauae.com/";
   static const String register = "$baseUrl/register";
   static const String login = "$baseUrl/login";
   static const String companiesIndex = "$baseUrl/apicompanies";
